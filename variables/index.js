43930a84a4c259e103cdd327c586142984b4a83f
Zeeson Manandhar <zeeson.manandhar11@gmail.com>
2024-10-23 18:59:33 +0545

    learned about variables

---
+++ variables/index.js
@@ -0,0 +1,22 @@
+//REPL (read, evaluate, print, loop)
+//Print
+console.log("Hello World");
+
+//Variables
+//named storage for data
+
+//ES5
+var person = "Zeeson";
+var sport = "Basketball";
+console.log(person,sport);
+
+//ES6
+//let, const
+
+const apple = "red apple";
+//apple = "Blue apple";
+console.log(apple);
+
+let ball = "red ball";
+ball = "yellow ball";
+console.log(ball);
