2daba528b162894fe8041f2db35a665cd9542c2c
Zeeson Manandhar <zeeson.manandhar11@gmail.com>
2024-10-25 18:36:28 +0545

    learned about operators

--- Statements/index.js
+++ Statements/index.js
@@ -22,43 +22,43 @@
 
 // // Switch case (Multiple Conditions)
 
-const day = prompt("Which day is today?");
+// const day = prompt("Which day is today?");
 
-switch (day) {
-  case "sun":
-    alert("Today is Sunday");
-    break;
+// switch (day) {
+//   case "sun":
+//     alert("Today is Sunday");
+//     break;
 
-  case "mon":
-    alert("Today is Monday");
-    break;
-  case "tues":
-    alert("Today is Tuesday");
-    break;
-  default:
-    alert("Invalid day");
-}
+//   case "mon":
+//     alert("Today is Monday");
+//     break;
+//   case "tues":
+//     alert("Today is Tuesday");
+//     break;
+//   default:
+//     alert("Invalid day");
+// }
 
-const marks = Number(prompt("Enter your grade:"));
-switch (true) {
-  case marks > 80 && marks < 100:
-    alert("Your grade is distinction");
-    break;
-  case marks > 70 && marks < 80:
-    alert("Your grade is A");
-    break;
+// const marks = Number(prompt("Enter your grade:"));
+// switch (true) {
+//   case marks > 80 && marks < 100:
+//     alert("Your grade is distinction");
+//     break;
+//   case marks > 70 && marks < 80:
+//     alert("Your grade is A");
+//     break;
 
-  case marks < 70:
-    alert("You are Failed");
-    break;
-}
+//   case marks < 70:
+//     alert("You are Failed");
+//     break;
+// }
 
 //ES6
 //Ternary Operator
 
 // write a ternary operator to alert gender male or female
 
-const myGender = prompt("What is your Gender?");
+// const myGender = prompt("What is your Gender?");
 
 // myGender ? alert("you are male") : ("You are Female");
 
@@ -69,10 +69,20 @@ const myGender = prompt("What is your Gender?");
 //g = o
 //g => random value
 
-myGender === "m"
-  ? alert("You are male")
-  : myGender === "f"
-  ? alert("you are Female.")
-  : myGender === "o"
-  ? alert("you are other")
-  : alert("jpt data");
+// myGender === "m"
+//   ? alert("You are male")
+//   : myGender === "f"
+//   ? alert("you are Female.")
+//   : myGender === "o"
+//   ? alert("you are other")
+//   : alert("jpt data");
+
+const marks = Number(prompt("Enter your grade"));
+
+marks >= 80 && marks <= 100
+  ? alert("Your grade is distinction")
+  : marks >= 70 && marks < 80
+  ? alert("Your grade is A")
+  : marks < 70
+  ? alert("you are failed")
+  : alert("Default input");
---
+++ operators/index.js
@@ -0,0 +1,25 @@
+//operators are used for handling multple conditions at once
+// syntax: AND, OR, NOT
+//Symbols: &&, ||, !
+
+// const hasNagarita = confirm("Do you have Citizenship?");
+// const age = confirm("Are you above 18 years old");
+
+// if (hasNagarita && age) {
+//   alert("YOU ARE ELIGIBLE FOR DRIVER LICENSE");
+// } else {
+//   alert("you are not eligible");
+// }
+
+// hasNagarita && age ? alert("You are eligible!") : alert("you are not eligible");
+
+// const hasNagarita = confirm("Do you have Citizenship?");
+// const hasNID = confirm("Do you have NID");
+// hasNagarita || hasNID
+//   ? alert("You are eligible!")
+//   : alert("you are not eligible");
+
+const user = prompt("What is your username?");
+
+user != "zeeson" ? alert(`Welcome ${user}`) : alert("Welcome Admin");
+
